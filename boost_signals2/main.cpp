18fd48f34ae2c2439ef1ed81599cc51838600396
HanatoK <summersnow9403@gmail.com>
2021-08-24 07:41:46 -0500

    Boost signals2 allows template

--- boost_signals2/main.cpp
+++ boost_signals2/main.cpp
@@ -2,9 +2,10 @@
 #include <boost/signals2.hpp>
 #include <functional>
 
+template <typename T>
 class Sender {
 public:
-  boost::signals2::signal<void(int)> m_foo; // not copy-constructible
+  boost::signals2::signal<void(T)> m_foo; // not copy-constructible
   Sender() {}
   Sender(const Sender& rhs) {}
   void send(int x) {
@@ -12,24 +13,25 @@ public:
   }
 };
 
+template <typename T>
 class Receiver {
 private:
   std::ostream& m_os;
 public:
   Receiver(std::ostream& os): m_os(os) {}
-  void receive(int x) {
+  void receive(T x) {
     m_os << x << std::endl;
   }
 };
 
 int main() {
-  Sender sender;
-  Receiver receiver(std::cout);
+  Sender<int> sender;
+  Receiver<int> receiver(std::cout);
   boost::signals2::connection c = sender.m_foo.connect(
-      std::bind(&Receiver::receive, &receiver, std::placeholders::_1));
+      std::bind(&decltype(receiver)::receive, &receiver, std::placeholders::_1));
   sender.send(100);
   sender.send(20);
-  Sender sender2(sender);
+  decltype(sender) sender2(sender);
   // no output
   sender2.send(30);
   return 0;

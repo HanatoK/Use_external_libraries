892ba3cce90ed63cebd9997ae1a91b8f2f3f93da
HanatoK <summersnow9403@gmail.com>
2023-02-27 15:38:09 -0600

    Break iterator test

--- icu_string/src/main.cpp
+++ icu_string/src/main.cpp
@@ -3,8 +3,10 @@
 #include <unicode/regex.h>
 #include <unicode/uregex.h>
 #include <unicode/utypes.h>
+#include <unicode/brkiter.h>
 #include <iostream>
 #include <string>
+#include <memory>
 
 int main() {
     const icu::UnicodeString s{"你好 世界\thello"};
@@ -78,9 +80,43 @@ int main() {
             std::cout << str_std << std::endl;
         }
     }
+    // grapheme
+    {
+        std::cout << std::string(10, '=') << " 05. Grapheme cluster " << std::string(10, '=') << std::endl;
+        const icu::UnicodeString s2 = icu::UnicodeString::fromUTF8("👨‍👩‍👧‍👦好!");
+        UErrorCode status = U_ZERO_ERROR;
+        const auto bi = std::unique_ptr<icu::BreakIterator>(
+            icu::BreakIterator::createCharacterInstance(icu::Locale::getDefault(), status));
+        if (U_FAILURE(status)) return 1;
+        bi->setText(s2);
+        int32_t p = bi->first();
+        size_t len = 0;
+        if (p != icu::BreakIterator::DONE) {
+            auto prev = p;
+            p = bi->next();
+            while (p != icu::BreakIterator::DONE) {
+                const icu::UnicodeString current_char(s2, prev, p - prev);
+                std::string current_char_std;
+                current_char.toUTF8String(current_char_std);
+                std::cout << current_char_std << std::endl;
+                ++len;
+                prev = p;
+                p = bi->next();
+            }
+        }
+        std::cout << "Length = " << len << std::endl;
+        // wrong
+        icu::StringCharacterIterator iter(s2);
+        for (auto c = iter.first(); c != icu::CharacterIterator::DONE; c = iter.next()) {
+            const icu::UnicodeString s1{c};
+            std::string s1_std;
+            s1.toUTF8String(s1_std);
+            std::cout << s1_std << std::endl;
+        }
+    }
     // test input
     {
-        std::cout << std::string(10, '=') << " 05. Input " << std::string(10, '=') << std::endl;
+        std::cout << std::string(10, '=') << " 06. Input " << std::string(10, '=') << std::endl;
         std::string input_str;
         std::cin >> input_str;
         const icu::UnicodeString ss = icu::UnicodeString::fromUTF8(input_str);
